1721adb248aba0cbc892aceb612fadcfb859e65e
Urban Faubion <urban.faubion@gmail.com>
2015-10-10 00:56:18 +0200

    [Changed] Now using ES7 Async methods in tests.

--- src/__tests__/index.js
+++ src/__tests__/index.js
@@ -8,9 +8,15 @@ import HtmlPlugin from '../'
 import rimraf from 'rimraf'
 import promisify from '@urban/promisify'
 
-const outputDir = join(__dirname, '../dist/')
-const outputFile = join.bind(null, outputDir)
-const removeOutput = rimraf.bind(null, outputDir)
+const outputDir = join(__dirname, './tmp/')
+const pathTo = join.bind(null, outputDir)
+const clear = promisify(rimraf.bind(null, outputDir))
+
+const build = promisify(webpack)
+const read = promisify(fs.readFile)
+const exists = promisify(fs.stat, function (err, result) {
+  this.resolve(err === null)
+})
 
 const config = {
   context: __dirname,
@@ -29,172 +35,152 @@ const config = {
   }
 }
 
-const webpackPromise = promisify(webpack)
-const readFilePromise = promisify(fs.readFile)
-
 function customTemplate (data) {
-  return `
-<!DOCTYPE html>
-<head>
-   <title>${data.title}</title>
-   <link rel="stylesheet" href="${data.css}"/>
-</head>
-<body>
-  <div id="content">
-   ${data.html}
-  </div>
-  <script src="${data.main}"></script>
-</body>`
+  return `<!DOCTYPE html>
+    <head>
+       <title>${data.title}</title>
+       <link rel="stylesheet" href="${data.css}"/>
+    </head>
+    <body>
+      <div id="content">
+       ${data.html}
+      </div>
+      <script src="${data.main}"></script>
+    </body>
+  </html>`
 }
 
-test('It work.', function (assert) {
+test('It work.', async assert => {
   assert.plan(3)
-  webpackPromise(Object.assign({}, config, {
-    plugins: [
-      ...config.plugins,
-      new HtmlPlugin()
-    ]
-  }))
-  .then(function (stats) {
+  try {
+    const stats = await build(Object.assign({}, config, {
+      plugins: [
+        ...config.plugins,
+        new HtmlPlugin()
+      ]
+    }))
     assert.pass('No fatal error.')
-    const jsonStats = stats.toJson()
-    assert.equal(jsonStats.errors.length, 0, 'No webpack errors.')
-    assert.equal(jsonStats.warnings.length, 0, 'No webpack warnings.')
-    removeOutput(assert.end)
-  })
-  .catch(console.log)
+    assert.equal(stats.hasErrors(), false, 'No webpack errors.')
+    assert.equal(stats.hasWarnings(), false, 'No webpack warnings.')
+  } catch (err) {
+    console.log(err)
+  }
+  await clear()
+  assert.end()
 })
 
-test('It generates a default index.html.', function (assert) {
-  assert.plan(4)
-  webpackPromise(Object.assign({}, config, {
-    plugins: [
-      ...config.plugins,
-      new HtmlPlugin()
-    ]
-  }))
-  .then(function (stats) {
-    assert.pass('No fatal error.')
-    return readFilePromise(outputFile('index.html'))
-  })
-  .then(function (data) {
-    assert.pass('File exists.')
-    const html = data.toString()
+test('It generates a default index.html.', async assert => {
+  assert.plan(2)
+  try {
+    await build(Object.assign({}, config, {
+      plugins: [
+        ...config.plugins,
+        new HtmlPlugin()
+      ]
+    }))
+    const html = (await read(pathTo('index.html'))).toString()
     const sourceRegExp = /<script src="main.js"><\/script>/
     assert.equal(html.match(sourceRegExp).length, 1, 'Has one JavaScript source.')
     const styleRegExp = /<link rel="stylesheet" href="main.css"\/>/
     assert.equal(html.match(styleRegExp).length, 1, 'Has one CSS stylesheet.')
-    removeOutput(assert.end)
-  })
-  .catch(console.log)
+  } catch (err) {
+    console.log(err)
+  }
+  await clear()
+  assert.end()
 })
 
-test('It allows custom HTML filename.', function (assert) {
-  assert.plan(2)
-  webpackPromise(Object.assign({}, config, {
-    plugins: [
-      ...config.plugins,
-      new HtmlPlugin(function (assets, defaultTemplate, compiler) {
-        return { 'test.html': defaultTemplate(assets) }
-      })
-    ]
-  }))
-  .then(function (stats) {
-    assert.pass('No fatal error.')
-    return readFilePromise(outputFile('test.html'))
-  })
-  .then(function (data) {
-    assert.pass('File exists.')
-    removeOutput(assert.end)
-  })
-  .catch(console.log)
-})
-
-test('It allows custom HTML template.', function (assert) {
-  assert.plan(3)
-  webpackPromise(Object.assign({}, config, {
-    plugins: [
-      ...config.plugins,
-      new HtmlPlugin(function (assets, defaultTemplate, compiler) {
-        const templateData = Object.assign({}, assets, {
-          title: 'Custom Template',
-          html: 'Hello World'
+test('It allows custom HTML filename.', async assert => {
+  assert.plan(1)
+  try {
+    await build(Object.assign({}, config, {
+      plugins: [
+        ...config.plugins,
+        new HtmlPlugin(function (assets, defaultTemplate, compiler) {
+          return { 'test.html': defaultTemplate(assets) }
         })
-        return { 'index.html': customTemplate(templateData) }
-      })
-    ]
-  }))
-  .then(function (stats) {
-    assert.pass('No fatal error.')
-    return readFilePromise(outputFile('index.html'))
-  })
-  .then(function (data) {
-    assert.pass('File exists.')
-    const html = data.toString()
+      ]
+    }))
+    assert.equal(await exists(pathTo('test.html')), true, 'Custom file exists.')
+  } catch (err) {
+    console.log(err)
+  }
+  await clear()
+  assert.end()
+})
+
+test('It allows custom HTML template.', async assert => {
+  assert.plan(1)
+  try {
+    await build(Object.assign({}, config, {
+      plugins: [
+        ...config.plugins,
+        new HtmlPlugin(function (assets, defaultTemplate, compiler) {
+          const templateData = Object.assign({}, assets, {
+            title: 'Custom Template',
+            html: 'Hello World'
+          })
+          return { 'index.html': customTemplate(templateData) }
+        })
+      ]
+    }))
+    const html = (await read(pathTo('index.html'))).toString()
     const contentRegExp = /<div id="content">[\s]*Hello World[\s]*<\/div>/
     assert.equal(html.match(contentRegExp).length, 1, 'Has custom content.')
-    removeOutput(assert.end)
-  })
-  .catch(console.log)
+  } catch (err) {
+    console.log(err)
+  }
+  await clear()
+  assert.end()
 })
 
-test('It allows async.', function (assert) {
+test('It allows async.', async assert => {
+  assert.plan(1)
+  try {
+    await build(Object.assign({}, config, {
+      plugins: [
+        ...config.plugins,
+        new HtmlPlugin(function (assets, defaultTemplate, compiler) {
+          return new Promise(function (resolve, reject) {
+            setTimeout(resolve, 250, { 'index.html': defaultTemplate(assets) })
+          })
+        })
+      ]
+    }))
+    assert.equal(await exists(pathTo('index.html')), true, 'File exists.')
+  } catch (err) {
+    console.log(err)
+  }
+  await clear()
+  assert.end()
+})
+
+test('It allows you to write multiple HTML files.', async assert => {
   assert.plan(2)
-  webpackPromise(Object.assign({}, config, {
-    plugins: [
-      ...config.plugins,
-      new HtmlPlugin(function (assets, defaultTemplate, compiler) {
-        return new Promise(function (resolve, reject) {
-          setTimeout(resolve, 250, { 'index.html': defaultTemplate(assets) })
+  try {
+    const indexTitle = 'Index File'
+    const testTitle = 'Test File'
+    await build(Object.assign({}, config, {
+      plugins: [
+        ...config.plugins,
+        new HtmlPlugin(function (assets, defaultTemplate, compiler) {
+          const indexData = Object.assign({}, assets, { title: indexTitle })
+          const testData = Object.assign({}, assets, { title: testTitle })
+          return {
+            'index.html': defaultTemplate(indexData),
+            'test.html': defaultTemplate(testData)
+          }
         })
-      })
-    ]
-  }))
-  .then(function (stats) {
-    assert.pass('No fatal error.')
-    return readFilePromise(outputFile('index.html'))
-  })
-  .then(function (data) {
-    assert.pass('File exists.')
-    removeOutput(assert.end)
-  })
-  .catch(console.log)
-})
-
-test('It allows you to write multiple HTML files.', function (assert) {
-  assert.plan(3)
-  webpackPromise(Object.assign({}, config, {
-    plugins: [
-      ...config.plugins,
-      new HtmlPlugin(function (assets, defaultTemplate, compiler) {
-        const indexData = Object.assign({}, assets, { title: 'Index File' })
-        const testData = Object.assign({}, assets, { title: 'Test File' })
-        return {
-          'index.html': defaultTemplate(indexData),
-          'test.html': defaultTemplate(testData)
-        }
-      })
-    ]
-  }))
-  .then(function (stats) {
-    assert.pass('No fatal error.')
-    return Promise.all([
-      readFilePromise(outputFile('index.html'))
-        .then(function (data) {
-          const html = data.toString()
-          const titleRegExp = /<title>Index File<\/title>/
-          assert.equal(html.match(titleRegExp).length, 1, 'File one has the right content.')
-        }),
-      readFilePromise(join(outputDir, 'test.html'))
-        .then(function (data) {
-          const html = data.toString()
-          const titleRegExp = /<title>Test File<\/title>/
-          assert.equal(html.match(titleRegExp).length, 1, 'File two has the right content.')
-        })
-    ])
-  })
-  .then(function () {
-    removeOutput(assert.end)
-  })
-  .catch(console.log)
+      ]
+    }))
+    const titleRegExp = /<title>(.*?)<\/title>/
+    const indexHtml = (await read(pathTo('index.html'))).toString()
+    assert.equal(indexHtml.match(titleRegExp)[1], indexTitle, 'File one has the correct content.')
+    const testHtml = (await read(pathTo('test.html'))).toString()
+    assert.equal(testHtml.match(titleRegExp)[1], testTitle, 'File two has the correct content.')
+  } catch (err) {
+    console.log(err)
+  }
+  await clear()
+  assert.end()
 })
